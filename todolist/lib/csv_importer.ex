fc981e81fce88a3f035ef2b80962b4af5de54ec3
Christo Mitov <christo.mitov@gmail.com>
2025-10-13 16:59:43 -0400

    formatting

--- todolist/lib/csv_importer.ex
+++ todolist/lib/csv_importer.ex
@@ -17,8 +17,12 @@ defmodule TodoList.CsvImporter do
     |> File.stream!()
     |> Stream.map(&String.trim_trailing(&1, "\n"))
     |> Stream.map(&String.split(&1, ","))
-    |> Stream.map(fn [date_string, title] -> [Date.from_iso8601!(date_string), title] end)
-    |> Stream.map(fn [date, title] -> %{date: date, title: title} end)
+    |> Stream.map(fn [date_string, title] ->
+      [Date.from_iso8601!(date_string), title]
+    end)
+    |> Stream.map(fn [date, title] ->
+      %{date: date, title: title}
+    end)
     |> Enum.to_list()
     |> TodoList.new()
   end
