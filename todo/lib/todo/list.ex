bdfc101bd7674731bc1b5b87ffff1f023b01b760
Christo Mitov <christo.mitov@gmail.com>
2025-10-19 19:24:23 -0400

    refactoring, multiple todo list support, genserver stuff

---
+++ todo/lib/todo/list.ex
@@ -0,0 +1,77 @@
+defmodule Todo.List do
+  defstruct next_id: 1, entries: %{}
+
+  @moduledoc """
+  Todolist abstraction
+  """
+
+  @doc """
+  Instantiates a brand new todolist
+  """
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %Todo.List{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  @doc """
+  Add an entry to a provided todolist
+  """
+  def add_entry(%Todo.List{} = todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.next_id)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
+    %Todo.List{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
+
+  @doc """
+  Get entries for a specific date
+  """
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(fn entry -> entry.date == date end)
+  end
+
+  @doc """
+  Update a specific TodoList entry by id
+  """
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        put_in(todo_list.entries[entry_id], new_entry)
+    end
+  end
+
+  @doc """
+  Delete a specific TodoList entry by id
+  """
+  def delete_entry(%Todo.List{} = todo_list, entry_id) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, _} ->
+        new_entries = Map.drop(todo_list.entries, [entry_id])
+        %Todo.List{todo_list | entries: new_entries}
+    end
+  end
+
+  defimpl Collectable, for: Todo.List do
+    def into(original) do
+      {original, &into_callback/2}
+    end
+
+    defp into_callback(todo_list, {:cont, entry}) do
+      Todo.List.add_entry(todo_list, entry)
+    end
+
+    defp into_callback(todo_list, :done), do: todo_list
+    defp into_callback(_todo_list, :halt), do: :ok
+  end
+end
---
+++ todo/lib/todo/server.ex
@@ -0,0 +1,40 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start() do
+    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def add_entry(pid, new_entry), do: GenServer.cast(pid, {:add_entry, new_entry})
+
+  def update_entry(pid, entry_id, updater_fun),
+    do: GenServer.cast(pid, {:update_entry, entry_id, updater_fun})
+
+  def delete_entry(pid, entry_id), do: GenServer.cast(pid, {:delete_entry, entry_id})
+  def entries(pid, date), do: GenServer.call(pid, {:entries, date})
+
+  @impl GenServer
+  def init(_) do
+    {:ok, Todo.List.new()}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  end
+
+  @impl GenServer
+  def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
+    {:noreply, Todo.List.update_entry(todo_list, entry_id, updater_fun)}
+  end
+
+  @impl GenServer
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    {:noreply, Todo.List.delete_entry(todo_list, entry_id)}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, Todo.List.entries(todo_list, date), todo_list}
+  end
+end
